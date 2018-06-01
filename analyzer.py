58ee61049edafa3379e6dd25c33b45a46e6696fa
vampirictutor <vampirictutor@gmail.com>
2018-06-02 00:56:21 +0800

    Yes, we can draw MA now.

--- analyzer.py
+++ analyzer.py
@@ -41,13 +41,15 @@ def array_content_to_file(the_file, var_name, the_array):
 
 
 
-def generate_js_data( jsfilename, sec1, sec2, logged_his):
+def generate_js_data_w_head( jsfilename,  logged_his):
     with io.open( jsfilename, "wb" ) as js_file:
         array_content_to_file( js_file, "header" , logged_his[0:1])
         array_content_to_file( js_file, "raw_data" , logged_his[1:])
 
-
-
+def generate_js_head_n_data( jsfilename, header, data ):
+    with io.open( jsfilename, "wb" ) as js_file:
+        array_content_to_file( js_file, "header" , [header])
+        array_content_to_file( js_file, "raw_data" , data)
 
 def write_chart_html_header( the_file, jsfilename, sec1, sec2):
     the_file.write(" <html>\n<head>\n<title>%s %s</title>\n" % ( str(sec1) ,str(sec2) ))
@@ -129,11 +131,41 @@ def html_end( the_file):
     the_file.write(" </body>\n</html>\n")
 
 
+def generate_htm_chart_3lines_w_annotation( sec1, sec2, bt ):
+    return 
+
+
+def generate_2lines_chart( line1_name, line2_name, data):
+    jsfilename = "%s_%s.js" % ( line1_name, line2_name)
+    jsfilepath = "%s/%s" % (data_struct.WORKING_DIR, jsfilename) 
+    generate_js_head_n_data (jsfilepath,  ['日期', line1_name, line2_name ] , data)
+ 
+    filename = "%s/%s_%s.html" % (data_struct.WORKING_DIR, line1_name, line2_name)
+    with io.open( filename, "wb" ) as the_file:
+        write_chart_html_header( the_file, jsfilename, line1_name, line2_name ) 
+        draw_chart_full( the_file, line1_name, line2_name  )
+
+        if len(data) > 1000:
+            draw_chart_last_x( the_file, line1_name, line2_name , 600 , 1 )
+            draw_chart_last_x( the_file, line1_name, line2_name , 300 , 2 )
+
+        head_end_body_begin( the_file)
+        chart_div_full(the_file)
+        
+        if len(data) > 1000:
+        
+            chart_div_subvar( the_file,1 )
+            chart_div_subvar( the_file,2 )
+
+        html_end( the_file)
+
+
+
 def generate_his_htm_chart( sec1, sec2, logged_his):
     
     jsfilename = "%s_%s.js" % ( sec1.code, sec2.code)
     jsfilepath = "%s/%s" % (data_struct.WORKING_DIR, jsfilename) 
-    generate_js_data(jsfilepath,  sec1, sec2, logged_his )
+    generate_js_data_w_head(jsfilepath,  logged_his )
  
     filename = "%s/%s_%s.html" % (data_struct.WORKING_DIR, sec1.code, sec2.code)
     with io.open( filename, "wb" ) as the_file:
@@ -160,7 +192,7 @@ def generate_diff_htm_chart( sec1, sec2, diff_his):
     
     jsfilename = "%s_%s.diff.js" % ( sec1.code, sec2.code)
     jsfilepath = "%s/%s" % (data_struct.WORKING_DIR, jsfilename) 
-    generate_js_data(jsfilepath,  sec1, sec2, diff_his )
+    generate_js_data_w_head(jsfilepath,   diff_his )
     
     filename = "%s/%s_%s.diff.html" % (data_struct.WORKING_DIR, sec1.code, sec2.code)
 
@@ -184,29 +216,6 @@ def generate_diff_htm_chart( sec1, sec2, diff_his):
         html_end( the_file)
 
 
-    #filename = "%s/%s_%s.diff.html" % (data_struct.WORKING_DIR, sec1.code, sec2.code)
-    #html_file = io.open( filename, "wb" )
-
-    #template_A_file = io.open( data_struct.TEMPL_A , "r") 
-    #template_A = str( template_A_file.read() )
-    #template_A_file.close()
-
-    #template_B_file = io.open( data_struct.TEMPL_B , "r") 
-    #template_B = str( template_B_file.read())
-    #template_B_file.close()
-
-    #html_file.write( 
-    #    template_A.replace( 
-    #        '$title$' , "diff of (%s - %s)" % (str(sec1), str(sec2))
-    #        )
-    #    )
-
-    #html_file.write( str( diff_his).decode('string_escape'))
-
-    #html_file.write( "\n")
-    #html_file.write( template_B.replace('$more_chart_options$', ',colors: [\'black\']' ) )
-
-    #html_file.close()
      
 def correlation( dbcur, sec1, sec2):
 
@@ -310,7 +319,134 @@ def correlation( dbcur, sec1, sec2):
 
     return ( r_close, r_delta, row_num )
 
+
+# 模拟换马策略
+# Input:  2-D array 'logged_his'
+#         日期   脚1对数化收盘价     脚2对数化收盘价     脚1收盘价  脚2收盘价
+#         ...
+#
+# Output: 2-D array 
+#         日期   脚1对数化收盘价     脚2对数化收盘价   策略对数化收盘价  换仓提示  换仓详细  
+#         ...
+#
+def sim_faster_horse( sec1, sec2, logged_his, MA_Size = 20):
+    #我在excel里根据50和500的昨收，算出ln(50昨收)-ln(500昨收)，称为ln差价，ln差价求20日移动平均。
+    #如果ln差价小于20日平均，持有500，反之，持有50。
+    #持有50的话，当日收益为ln(50收盘)-ln(50昨收)，否则，为500的收益（当然，换股日的话，收益要减去0.0002。因为假设有万分之二的etf交易手续费）。
+    #然后，将50、500、持仓的每日收益分别累积，就画出了你看到的三根线。
+
+    # 2D 数组 :
+    #  日期, ln差价,  MA(20) of  ln差价
+    indices = []
+    MA_Sum = 0
+    for index, row in enumerate( logged_his  ):
+
+        the_diff =  (row[1] -row[2])
+
+        if index <= (MA_Size - 1): 
+            
+            #累加的窗口
+            MA_Sum = MA_Sum + the_diff
+
+
+            #部分MA
+            MA = MA_Sum / (index + 1)
+
+            indices.append( 
+                [
+                    row[0]
+                    , the_diff 
+                    , MA
+                ] )
+        else:
+            # 以后的MA，窗口向后滑动一格
+            MA_Sum = MA_Sum - indices[index - MA_Size][1] +  the_diff
+            
+            #计算MA
+            MA = MA_Sum / MA_Size
+            
+            indices.append( 
+                [
+                    row[0]
+                    , the_diff
+                    , MA
+                ] )
+
+    generate_2lines_chart( "ln%s-%s" % (str(sec1.code), str(sec2.code) )
+            , "MA%d" % MA_Size 
+            , indices[0:200] )
+
+
+def bt_faster_horse( dbcur, sec1, sec2):
+
+    dbcur.execute ('''select a.t_day, a.close, a.delta_r, b.close, b.delta_r 
+        from MdHis a , MdHis b
+        where 
+            a.code = ? and b.code = ?
+            and a.t_day = b.t_day
+            and a.delta_r is not null and b.delta_r is not null
+        order by a.t_day 
+        '''
+        , (sec1.code ,sec2.code)
+        )
    
+
+    v_c1 = []
+    v_c2 = []
+
+    logged_his= []
+
+    #fieldnames = ['T_Day', str(sec1), str(sec2) , '' , '' ]
+    #logged_his.append( fieldnames );
+
+    row_num = 0
+
+    first_c1 = 0
+    first_c2 = 0
+
+    row = dbcur.fetchone()
+    while row is not None:
+
+        row_num = row_num + 1
+
+        close1 = float(row[1] )
+        close2 = float(row[3] )
+
+        if 1 == row_num:
+            # (相对于期初)对数化
+            logged_his.append( [row[0], 0, 0,  close1, close2 ] )
+            first_c1 = math.log(close1)
+            first_c2 = math.log(close2)
+
+        else:
+            # (相对于期初)对数化
+            logged1 = math.log(close1) - first_c1  
+            logged2 = math.log(close2) - first_c2 
+            logged_his.append( [
+                row[0]
+                , logged1 
+                , logged2
+                , close1
+                , close2
+                 ] )
+
+        row = dbcur.fetchone()
+
+
+    #generate_his_csv( code1, code2, logged_his)
+    #generate_his_htm_chart( sec1, sec2, logged_his[:100])
+    bt = sim_faster_horse(sec1,sec2, logged_his, 5)
+    bt = sim_faster_horse(sec1,sec2, logged_his, 10)
+    bt = sim_faster_horse(sec1,sec2, logged_his, 20)
+
+    generate_htm_chart_3lines_w_annotation( sec1, sec2, bt )
+    
+    #net_value  =  math.exp( bt[len(bt)][3] ) 
+    net_value  =  0
+
+    return (net_value , row_num )
+
+  
 def cmp_correl( correl1, correl2):
     k1 = correl1.r_close + correl1.r_delta 
     k2 = correl2.r_close + correl2.r_delta 
