ecf0f7b7e86faaaf83b3bd84673c8abe783b34b7
vampirictutor <vampirictutor@gmail.com>
2018-06-02 22:30:32 +0800

    sample of 'line chart with annotation'

---
+++ html_template/line_sample.w_anno.data.js
@@ -0,0 +1,17 @@
+var header = [ 
+        ['Year', 'Sales', 'Expenses']  
+    ];
+
+var raw_data = [
+    ['2004',  1000,      400 , null, null ],
+    ['2005',  1170,      460 , null, null ],
+    ['2006',  660,       1120, ":)", "Hahahaha" ],
+    ['2007',  1030,      540 , null, null ] 
+    ,['2008',  900,      480 , null, null ]
+    ,['2009',  1070,     490 , ":(", "WTF!" ]
+    ,['2010',  690,      920 , null, null ]
+    ,['2011',  990,      780 , null, null ]
+
+  ];
+
+     
